c2bce01e8b16be724954581c16b9550d23871016
Julian <jgarcg@uc.cl>
2023-02-18 19:52:34 -0300

    feat: stacked plot for spendings

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,22 +9,24 @@
     let incomes: Income[] = [];
     let spendings: Spending[] = [];
 
-    let chart: typeof import("@carbon/charts-svelte").AreaChart;
+    let chart: typeof import("@carbon/charts-svelte").StackedAreaChart;
 
     onMount(async () => {
         db = await createDbConnection();
         const charts = await import("@carbon/charts-svelte");
-        chart = charts.AreaChart;
+        chart = charts.StackedAreaChart;
     })
 
     async function loadIncomesFromDatabase() {
         if(db) {
             incomes = await getIncomesFromDatabase(db)
+            console.log(incomes)
         }
     }
     async function loadSpendingsFromDatabase() {
         if(db) {
             spendings = await getSpendingsFromDatabase(db)
+            console.log(spendings)
         }
     }
 
@@ -38,40 +40,118 @@
           "title": "Fecha",
           "mapsTo": "date",
           "scaleType": "time" as ScaleTypes,
-			    "stacked": true,
-          "ticks": {
-            "formatter": (date: number) => new Date(date).toLocaleDateString()
-          }
-        },
+          // "ticks": {
+          //   "formatter": (date: number) => new Date(date).toLocaleDateString()
+          // }
+        },		
         "left": {
-          "mapsTo": "value",
-          "title": "Acumulado (pesos)",
-          "scaleType": "linear" as ScaleTypes
-        }
+          "stacked": true,
+          "title": "Dinero (pesos)"
+        },
       },
       "curve": "curveMonotoneX",
-      "height": "400px"
+      "height": "400px",
+      "timeScale": {
+        "addSpaceOnEdges": 1
+      },
+      "color": {
+        "pairing": {
+          "option": 2,
+        },
+        "scale": {
+          "Ingreso": "#0F4392",
+          "Gasto": "#FF4949",
+        }
+      },
     }
 
-    function getCumulativePerDay(arr: Income[] | Spending[], type: string) {
+    function getCumulativePerDay(incomes: Income[], spendings: Spending[]) {
         const ret: {group: string, date: number, value: number}[] = [];
-        arr.forEach((val, idx) => {
-            const date = new Date(val.timestamp).setHours(0, 0, 0, 0);
-            if(idx == 0 || ret[ret.length - 1].date != date) {
-                ret.push({
-                    group: type,
-                    date: date,
-                    value: val.value
-                })
-            } else {
-                ret[ret.length - 1].value += val.value
-            }
+        
+        const datesSet = new Set([
+          ...incomes.map((d) => d.timestamp),
+          ...spendings.map((d) => d.timestamp),
+        ]);
+
+        const dates = Array.from(datesSet).sort(); // esto se puede hacer más rápido
+
+        let incomeIdx = 0;
+        let spendingIdx = 0;
+
+        const perDate = dates.map((date) => {
+          let income = 0;
+          let spending = 0;
+          if(incomeIdx < incomes.length && incomes[incomeIdx].timestamp == date) {
+            income = incomes[incomeIdx].value;
+          }
+          if(spendingIdx < spendings.length && spendings[spendingIdx].timestamp == date) {
+            spending = spendings[spendingIdx].value;
+          }
+          return {
+            income,
+            spending,
+            date
+          }
         })
+
+        const cumulativePerDate: {income: number, spending: number, date: number}[] = [];
+
+        perDate.forEach((val, i, arr) => {
+          cumulativePerDate.push({
+            income: i == 0 ? val.income : val.income + cumulativePerDate[cumulativePerDate.length-1].income,
+            spending: i == 0 ? val.spending : val.spending + cumulativePerDate[cumulativePerDate.length-1].spending,
+            date: val.date
+          })
+        })
+
+        cumulativePerDate.forEach((val) => {
+          ret.push({
+            group: "Ingreso",
+            value: val.income,
+            date: val.date
+          })
+          ret.push({
+            group: "Gasto",
+            value: -val.spending,
+            date: val.date
+          })
+        })
+
         return ret;
+
+
+        // arr.forEach((val, idx) => {
+        //     const date = new Date(val.timestamp).getTime();
+        //     // if(idx == 0 || ret[ret.length - 1].date != date) {
+        //     //     ret.push({
+        //     //         group: type,
+        //     //         date: date,
+        //     //         value: idx == 0 ? val.value : val.value + ret[ret.length - 1].value
+        //     //     })
+        //     // } else {
+        //     //     ret[ret.length - 1].value += val.value
+        //     // }
+        //     let value;
+        //     if(type == "Gastos") {
+        //       value = - val.value;
+        //     } else {
+        //       value = val.value;
+        //     }
+        //     ret.push({
+        //         group: type,
+        //         date: date,
+        //         value: idx == 0 ? value : value + ret[ret.length - 1].value
+        //     })
+        // })
+        // ret.push({
+        //   group: type,
+        //   date: Date.now(),
+        //   value: ret.length != 0 ? ret[ret.length - 1].value : 0
+        // })
+        // return ret;
     }
 
-    $: incomesPerDay = getCumulativePerDay(incomes, "Ingresos")
-    $: spendingsPerDay = getCumulativePerDay(spendings, "Gastos")
+    $: allBudget = getCumulativePerDay(incomes, spendings)
 </script>
 
 <h1 class="text-2xl text-gray-700 font-bold mb-2">Resumen de Gastos</h1>
@@ -79,7 +159,7 @@
 <div class="px-4">
   <svelte:component
       this={chart}
-      data={incomesPerDay.concat(spendingsPerDay)}
+      data={allBudget}
       options={fullOptions} 
   />
 </div>
